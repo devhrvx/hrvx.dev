4258f04cac13d86a0f80517b6316e5740a1a5306
hrvyCased <salinas.johnharvey11@gmail.com>
2024-07-06 18:55:03 +0800

    style: Update navbar and name-container styles

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function openNav() {
     document.querySelector('.all').classList.add('blur');
     document.querySelector('#logo').classList.add('blur');
     document.querySelector('.open-btn').classList.add('blur');
+    document.addEventListener('click', outsideClickListener);
 }
 
 function closeNav() {
@@ -12,9 +13,18 @@ function closeNav() {
     document.querySelector('.all').classList.remove('blur');
     document.querySelector('#logo').classList.remove('blur');
     document.querySelector('.open-btn').classList.remove('blur');
+    document.removeEventListener('click', outsideClickListener);
+}
+
+function outsideClickListener(event) {
+    const nav = document.querySelector('#nav');
+    const openBtn = document.querySelector('.open-btn');
+    if (!nav.contains(event.target) && !openBtn.contains(event.target)) {
+        closeNav();
+    }
 }
 
 function redirHome() {
-    window.location.href = 'index.html';
+    window.location.href = '/index.html';
 }
 
