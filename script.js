e68f94a81eb55c1d815d6bef6676c0d06da9bf2f
hrvyCased <salinas.johnharvey11@gmail.com>
2024-07-06 11:56:18 +0800

    added fade-in and many more nav bar ehehehe

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.querySelector('#nav').style.width = '250px';
+    document.querySelector('.nav').style.paddingLeft = '10px';
+}
+
+function closeNav() {
+    document.querySelector('#nav').style.width = '0';
+    document.querySelector('.nav').style.paddingLeft = '0';
+}
