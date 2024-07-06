3698a235f9fdbf945abedcbecece6ac9e0516f12
hrvyCased <salinas.johnharvey11@gmail.com>
2024-07-06 12:53:58 +0800

    style: Update styles.css and index.css for navbar and name-container

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 function openNav() {
     document.querySelector('#nav').style.width = '250px';
     document.querySelector('.nav').style.paddingLeft = '10px';
+    document.querySelector('.all').classList.add('blur');
+    document.querySelector('#logo').classList.add('blur');
+    document.querySelector('.open-btn').classList.add('blur');
 }
 
 function closeNav() {
     document.querySelector('#nav').style.width = '0';
     document.querySelector('.nav').style.paddingLeft = '0';
+    document.querySelector('.all').classList.remove('blur');
+    document.querySelector('#logo').classList.remove('blur');
+    document.querySelector('.open-btn').classList.remove('blur');
 }
