01774cece699373dd34095319625155c07443bd4
hrvyCased <salinas.johnharvey11@gmail.com>
2024-07-20 16:39:20 +0800

    swiper

--- connect/connect.js
+++ connect/connect.js
@@ -1,3 +1,45 @@
+
+
+
 function openLink(path) {
     window.open(path, '_blank');
 }
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const swiper = new Swiper('.slider-wrapper', {
+        loop: true,
+        grabCursor: true,
+        spaceBetween: 50,
+  
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+          dynamicBullets: true,
+        },
+  
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        
+        breakpoints: {
+            0: {
+                slidesPerView: 1
+            },
+            600: {
+                slidesPerView: 2
+            },
+            1024: {
+                slidesPerView: 3
+            },
+        }
+    });
+
+    
+
+});
+
+  
