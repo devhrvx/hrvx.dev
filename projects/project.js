66b27a81e763834887a8bb2f96f3197510914d66
hrvyCased <salinas.johnharvey11@gmail.com>
2024-07-25 08:50:08 +0800

    Swiper for minor projects slider

---
+++ projects/project.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const swiper = new Swiper('.swiper', {
+        slidesPerView: 2,
+        loop: true,
+        spaceBetween: 15,
+      
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+            },
+            800: {
+                slidesPerView: 1,
+            },
+            1000: {
+                slidesPerView: 1,
+            },
+            1200: {
+                slidesPerView: 2,
+            }
+        },
+    });
+});
